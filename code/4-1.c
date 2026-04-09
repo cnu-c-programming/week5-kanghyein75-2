b8cfa585c2debd3edc3c46087af6d702794e6b46
kanghyein75 <kanghye675@gmail.com>
2026-04-09 23:53:39 +0900

    Update 4-1.c

---
+++ code/4-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void echo() {
+    char buf[5];
+    scanf("%4s", buf);
+    printf("%s\n", buf);
+}
+
+int main() {
+    int x = 42;
+    ech0();
+    return 0;
+}
